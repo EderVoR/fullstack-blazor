236944a786ac47c554d5ed002f7d408553e66125
Eder Vieira <eder.edy@gmail.com>
2025-11-02 18:44:18 -0300

    Realizado Mapping das classes, adicionados pacotes Entity Framework e Swagger

---
+++ Dima/Dima.Api/Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,31 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        void IEntityTypeConfiguration<Category>.Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Description)
+                .IsRequired(false)
+                .HasColumnType("varchar")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(1600);
+        }
+    }
+}
---
+++ Dima/Dima.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,39 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        void IEntityTypeConfiguration<Transaction>.Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transaction");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Type)
+                .HasColumnType("smallint");
+
+            builder.Property(x => x.Amount)
+                .IsRequired()
+                .HasColumnType("decimal");
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired(true);
+
+            builder.Property(x => x.PaidOrReceivedAt)
+                .IsRequired(false);
+            
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(160);
+        }
+    }
+}

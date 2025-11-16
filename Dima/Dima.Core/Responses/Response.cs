5673650126305bcf8de9767371db3195bcaf47ad
Eder Vieira <eder.edy@gmail.com>
2025-11-16 17:12:07 -0300

    Criado reponse, request e paginação

---
+++ Dima/Dima.Core/Responses/Response.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses
+{
+    public class Response<TData>
+    {
+        [JsonConstructor]
+        public Response()
+            => _code = Configuration.DefaultStatusCode;
+
+        public Response(TData data, int code = Configuration.DefaultStatusCode, string message = null)
+        {
+            _code = code;
+            Data = data;
+            Message = message;
+        }
+
+        private readonly int _code;
+        public TData? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccess 
+            => _code is >= 200 and <= 299;
+    }
+}

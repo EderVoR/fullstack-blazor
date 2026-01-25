75406f81f46719adf31f9e17593f5a5057dfc6f1
Eder Vieira <eder.edy@gmail.com>
2026-01-25 20:17:34 -0300

    Ajustando mapeamento de rotas da Categoria.

--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -1,4 +1,5 @@
 using Dima.Api.Data;
+using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
 using Dima.Core.Handlers;
 using Dima.Core.Models;
@@ -29,52 +30,53 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "OK");
+app.MapEndpoints();
 
-app.MapPost("v1/categories", (CreateCategoryRequest request, ICategoryHandler handler) 
-    => handler.CreateAsync(request))
-    .WithName("Categories: Create")
-    .WithSummary("Cria uma nova categoria")
-    .Produces<Response<Category>>();
+//app.MapPost("v1/categories", (CreateCategoryRequest request, ICategoryHandler handler) 
+//    => handler.CreateAsync(request))
+//    .WithName("Categories: Create")
+//    .WithSummary("Cria uma nova categoria")
+//    .Produces<Response<Category>>();
 
-app.MapPut("v1/categories/{id}", async ([FromRoute] long id, UpdateCategoryRequest request, ICategoryHandler handler)
-    =>
-    {
-        request.Id = id;
-        return await handler.UpdateAsync(request);
-    })
-    .WithName("Categories: Update")
-    .WithSummary("Atualiza uma categoria")
-    .Produces<Response<Category?>>();
+//app.MapPut("v1/categories/{id}", async ([FromRoute] long id, UpdateCategoryRequest request, ICategoryHandler handler)
+//    =>
+//    {
+//        request.Id = id;
+//        return await handler.UpdateAsync(request);
+//    })
+//    .WithName("Categories: Update")
+//    .WithSummary("Atualiza uma categoria")
+//    .Produces<Response<Category?>>();
 
-app.MapDelete("v1/categories/{id}", async ([FromRoute] long id, ICategoryHandler handler)
-    =>
-    {
-        var request =  new DeleteCategoryRequest { Id = id };
-        return await handler.DeleteAsync(request);
-    })
-    .WithName("Categories: Delete")
-    .WithSummary("Remove uma categoria")
-    .Produces<Response<Category?>>();
+//app.MapDelete("v1/categories/{id}", async ([FromRoute] long id, ICategoryHandler handler)
+//    =>
+//    {
+//        var request =  new DeleteCategoryRequest { Id = id };
+//        return await handler.DeleteAsync(request);
+//    })
+//    .WithName("Categories: Delete")
+//    .WithSummary("Remove uma categoria")
+//    .Produces<Response<Category?>>();
 
-app.MapGet("v1/categories/{id}", async ([FromRoute] long id, ICategoryHandler handler)
-    =>
-    {
-        var request = new GetCategoryByIdRequest { Id = id };
-        return await handler.GetByIdAsync(request);
-    })
-    .WithName("Categories: Get By ID")
-    .WithSummary("Consulta uma categoria")
-    .Produces<Response<Category>>();
+//app.MapGet("v1/categories/{id}", async ([FromRoute] long id, ICategoryHandler handler)
+//    =>
+//    {
+//        var request = new GetCategoryByIdRequest { Id = id };
+//        return await handler.GetByIdAsync(request);
+//    })
+//    .WithName("Categories: Get By ID")
+//    .WithSummary("Consulta uma categoria")
+//    .Produces<Response<Category>>();
 
-app.MapGet("v1/categories", async (ICategoryHandler handler)
-    =>
-    {
-        var request = new GetAllCategoryRequest();
-        return await handler.GetAllAsync(request);
-    })
-    .WithName("Categories: Get All")
-    .WithSummary("Retorna todas as categorias")
-    .Produces<PagedResponse<Category>?>();
+//app.MapGet("v1/categories", async (ICategoryHandler handler)
+//    =>
+//    {
+//        var request = new GetAllCategoryRequest();
+//        return await handler.GetAllAsync(request);
+//    })
+//    .WithName("Categories: Get All")
+//    .WithSummary("Retorna todas as categorias")
+//    .Produces<PagedResponse<Category>?>();
 
 app.Run();
